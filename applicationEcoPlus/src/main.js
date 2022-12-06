1922044438938f4d43296eca4f50f7ce69ede431
Bernardolgm99 <bernardolgm99@gmail.com>
2022-12-06 12:08:48 +0000

    init project

---
+++ applicationEcoPlus/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+	components,
+	directives,
+});
+
+app.use(vuetify).mount('#app');
