d21fa963f077b067424b587eaf047dd5c6aa2872
NarizConstipado <gaspar.antunesvieira@gmail.com>
2023-01-08 16:30:29 +0000

    landingPage almost

--- applicationEcoPlus/src/views/LandingPageView.vue
+++ applicationEcoPlus/src/views/LandingPageView.vue
@@ -11,9 +11,9 @@
                     </v-col>
                     <v-col cols="2" >
                         <v-row id="entry">
-                            <span @click="this.$router.push('/home')" id="signs">Sign In</span>
+                            <span @click="this.$router.push('/signin')" id="signs">Sign In</span>
                             <span class="whiteText">|</span>
-                            <span @click="this.$router.push('/home')" id="signs">Log In</span>
+                            <span @click="this.$router.push('/signup')" id="signs">Log In</span>
                         </v-row>
                     </v-col>
                 </v-row>
@@ -21,18 +21,40 @@
         </div>
     <!-- register now and email input -->
         <v-container class="content">
-            <v-col >
+            <v-col>
                 <v-row class="content">
                     <span class="bgWhiteText">REGISTER NOW</span>
                 </v-row>
                 <v-row id="inputEmail">
                     <v-text-field id="textInput"
                     :rules="rules"
+                    v-model="inputEmail"
                     hide-details="auto"
+                    persistent-hint
+                    outlined
                     ></v-text-field>
-                    <button class="whiteText" id="btnStarted">Get Started ></button>
+                    <button @click="this.$router.push('/signup')" class="whiteText" id="btnStarted">Get Started ></button>
                 </v-row>
             </v-col>
+        </v-container>
+    </div>
+    <div id="textContent">
+        <div class="textContentInit">
+            <p>Welcome to <strong>Eco+</strong></p>
+            <p>We inspire to create a <strong>helpful tool</strong> to <strong>Eco-Escolas</strong> and develop a <strong>helping and caring community.</strong></p>
+        </div>
+        <v-container>
+            <v-row class="textContentWhite">
+                <v-col cols="7" class="textWhite">
+                    <p>We came to existence trough a proposal to <strong>Eco-Escolas. 
+                       Eco+ consists</strong> in a <strong>web application to assist</strong> members of <strong>Eco-Escolas</strong>, youngsters and older folks,
+                       to enrol themselves in activities. Trough our app you can also <strong>report 
+                       occurrences and propose to become a member of Eco-Escolas.</strong></p>
+                </v-col>
+                <v-col cols="5">
+                    <img src="../assets/landingPage/imageFirst.png" alt="Person at a computer">
+                </v-col>
+            </v-row>
         </v-container>
     </div>
     <div>
@@ -45,13 +67,14 @@
         data: () => ({
             rules: [
               value => !!value || 'Required.',
-              value => (value || '').length <= 20 || 'Max 20 characters',
+              value => (value || '').length <= 100 || 'Max 100 characters',
               value => {
                 const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                 return pattern.test(value) || 'Invalid e-mail.'
               },
             ],
-    }),
+            inputEmail: '',
+        }),
     }
 </script>
 
