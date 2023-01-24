9405b10066d46260861eae4f0e360a4e351b0c40
Eduardo Alves <edualves884@gmail.com>
2023-01-24 21:36:08 +0000

    design homePage, occurrenceMainPage, eventMainPage

--- applicationEcoPlus/src/views/OccurrencesView.vue
+++ applicationEcoPlus/src/views/OccurrencesView.vue
@@ -10,30 +10,44 @@
           </v-sheet>
         </v-col>
         <v-col>
-          <v-sheet class="pa-2 ma-2" style="background-color: blue">
+          <v-sheet class="pa-2 ma-2" style="background-color: rgba(255, 250, 246, 1); 
+          border-right: 1px solid rgba(111, 190, 135, 1);
+          border-left: 1px solid rgba(111, 190, 135, 1)">
             <!-- content -->
             <div v-for="post in feed" class="content">
 
               <div class="card">
-                <img :src=post.image width="500">
-                <div>
-                  <h1>{{ post.title }}</h1> &nbsp;
-                  <h3>{{ post.location }}</h3>
-                </div>
+                  <div class="image"  :style="`background: url(${post.image});`">
+                      <div class="topperIconsOccurrence">
+                        <div class="infoCardContent">
+                          <div class="postIconBackground">
+                            <img class="postIcon" :src="'src/assets/icons/tool.svg'">
+                          </div>
+                          <div class="infos">
+                            <div class="title">
+                              <span class="textMediumLarge">{{ post.title }}</span> &nbsp;
+                            </div>
+                            <div class="location">
+                              <span class="textSmall txtLocation">{{ post.location }}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                  </div>
               </div>
-
-              <div class="comments" v-for=",i in post.comments.length > 1 ? 2 : post.comments.length == 1 ? 1 : 0">
-                <p class="userName">@{{ userStore.getUserById(post.comments[i].userId).username }}</p>
-                <p class="comment">{{ post.comments[i].message }}</p>
-              </div>
-
-              <div class="routerLink">
-                <h5>
-                  <RouterLink :to="{ name: 'occurrenceDetail', params: { occurrenceid: post.id } }">View More!
-                  </RouterLink>
-                </h5>
-              </div>
-
+              <div class="comments" v-for=",i in post.comments.length > 1 ? 2 : (post.comments.length == 1 ? 1 : 0)">
+                    <p class="userName">@{{ userStore.getUserById(post.comments[i].userId).username }}: &nbsp;</p>
+                    <p class="comment">{{ post.comments[i].message }}</p>
+                  </div>
+                  <div class="routerLink">
+                    <p class="viewMore textSmall">
+                      <RouterLink :to="{ name: 'occurrenceDetail', params: { occurrenceid: post.id } }">- View More -
+                      </RouterLink>
+                    </p>
+                  </div>
+                  <div>
+                    <hr class="line">
+                  </div>
             </div>
           </v-sheet>
         </v-col>
@@ -80,7 +94,7 @@ export default {
         this.feed.push(occurrence)
       }
 
-      this.feed.sort((a, b) => (b.dateHour.date + b.dateHour.hour) - (a.dateHour.date + a.dateHour.hour))
+      this.feed.sort((a, b) => (b.dateHour.compare + b.dateHour.compare) - (a.dateHour.compare + a.dateHour.compare))
 
     }
   }
@@ -89,6 +103,7 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/styles/base.css';
+@import '../assets/styles/home.css';
 
 #inspire {
   scroll-behavior: smooth;
