75573cd8a62c93526ab621a2a183fd568e91e544
Bernardolgm99 <bernardolgm99@gmail.com>
2023-06-15 14:56:54 +0100

    home correction

--- applicationEcoPlus/src/stores/eventOccurrence.js
+++ applicationEcoPlus/src/stores/eventOccurrence.js
@@ -9,17 +9,19 @@ export const eventOccurrenceStore = defineStore('eventOccurrence', {
   state: () => ({
     eventsOccurrences: [],
     page: 0,
-    limit: 3
+    limit: 3,
+    createdAt: new Date().toISOString()
   }),
   getters: {
     getEventsOccurrences: (state) => state.eventsOccurrences,
   },
   actions: {
     async fetchAllEventsOccurrences(pagination = false) {
-      console.log("teste", this.page, this.limit)
-      await axios.get(`${API}/eventsOccurrences?limit=${this.limit}&page=${this.page}`).then((response) => { this.eventsOccurrences = this.eventsOccurrences.concat(response.data); });
-      console.log(this.eventsOccurrences);
-      this.page += this.limit;
+      await axios.get(`${API}/eventsOccurrences?limit=${this.limit}&page=${this.page}&createdAt=${this.createdAt}`)
+      .then((response) => { 
+        this.eventsOccurrences = this.eventsOccurrences.concat(response.data);
+        this.createdAt = this.eventsOccurrences[this.eventsOccurrences.length-1].createdAt
+      });
     },
   },
 })
